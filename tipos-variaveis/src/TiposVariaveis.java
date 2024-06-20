f5fc2d1006067c43c15098033535b5517859c759
rodrigo wenzel <rodrigocw90@gmail.com>
2024-06-20 11:05:21 -0300

    feat: inserido classe tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,5 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+        System.out.println("Olá Mundo");
     }
 }
