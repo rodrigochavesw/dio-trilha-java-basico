ebd10d30f47a22a3ebf3641db48c7d705378cef0
rodrigo wenzel <rodrigocw90@gmail.com>
2024-07-01 18:16:19 -0300

    feat: inserido classe tipos de variaveis, mensagem olá mundo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
+        System.out.println("Olá Mundo");
+        System.out.println("Olá Mundo");
+        System.out.println("Olá Mundo");
+        System.out.println("Olá Mundo");
+        System.out.println("Olá Mundo");
+
     }
 }
